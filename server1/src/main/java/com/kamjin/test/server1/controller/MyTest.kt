a5a4b04ff4956881b5504ddf725a1dd7668898d4
kamjin1996 <kamjin1996@163.com>
2021-09-24 14:59:56 +0800

    build

--- server1/src/main/java/com/kamjin/test/server1/controller/MyTest.kt
+++ server1/src/main/java/com/kamjin/test/server1/controller/MyTest.kt
@@ -1,16 +1,16 @@
-package com.kamjin.test.server2.controller
+package com.kamjin.test.server1.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RequestMapping("/server2/")
+@RequestMapping("/server1/")
 @RestController
 class MyTest {
 
-    @GetMapping("world")
-    fun world(@RequestParam("str") str: String): String {
+    @GetMapping("hello")
+    fun hello(@RequestParam("str") str: String): String {
         println(str)
         return "$str test"
     }
