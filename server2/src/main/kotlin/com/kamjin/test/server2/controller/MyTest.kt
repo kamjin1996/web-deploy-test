c8f9a4facae6fff48835557f283bf302ee0b34f3
kamjin1996 <kamjin1996@163.com>
2021-09-27 17:37:39 +0800

    valuam

--- server2/src/main/kotlin/com/kamjin/test/server2/controller/MyTest.kt
+++ server2/src/main/kotlin/com/kamjin/test/server2/controller/MyTest.kt
@@ -12,6 +12,6 @@ class MyTest {
     @GetMapping("world")
     fun world(@RequestParam("str") str: String): String {
         println(str)
-        return "$str test111"
+        return "$str test11111"
     }
 }
